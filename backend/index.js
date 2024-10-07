22da37424e21475fe203649a7191242e54235c39
Suraj Sharma <suraj200delhi@gmail.com>
2024-10-08 02:09:34 +0530

    Fix minor issue and add /:shortId endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,44 @@
 const express = require("express");
-const mongoose = require("mongoose");   
-require("dotenv").config(); 
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const app = express()
+const app = express();
 const port = 3000;
 const host = "localhost";
-const urlRoute = require('./routes/url');
+const urlRoute = require("./routes/url");
+const UrlModel = require("./database/db");
 
-const connectionString = process.env.MONGODB_URI
+const connectionString = process.env.MONGODB_URI;
 mongoose.connect(connectionString);
 
 app.use(express.json());
 
-app.use('/short',urlRoute)
+app.use("/short", urlRoute);
+
+app.post("/:shortId", async (req, res) => {
+  const id = req.params.shortId;
+  console.log(id);
+  
+  if (!id) {
+    return res.status(400).send({
+      message: " Incorrect format ",
+    });
+  }
+  try {
+    const existingId = await UrlModel.findOne({ shortUrl: id });
+    if (!existingId) {
+      return res.status(404).send({
+        message: "Not found",
+      });
+    }
+    const redirectUrl = existingId.mainurl;
+    res.redirect(redirectUrl);
+  } catch (error) {
+    return res.status(500).send({
+      message: `Error : ${error}`,
+    });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running at http://${host}:${port}`);
--- backend/routes/url.js
+++ backend/routes/url.js
@@ -2,34 +2,45 @@ const express = require("express");
 const router = express.Router();
 const UrlModel = require("../database/db");
 const shortUuid = require("short-uuid");
+const app = express();
 
-router.post("/:url", async (req, res) => {
-    try{
-        const {inputUrl} = req.param;
-        const existingUrl = await UrlModel.findOne({mainurl:inputUrl})
-        if(existingUrl){
-           return res.send({
-                message:`Short URL already exist${existingUrl.shortUrl}`
-            })
-        }else{
-            
-            const shortId = shortUuid.generate();
-            await UrlModel.create({
-                mainurl :inputUrl,
-                shortUrl:shortId
-            })
-            return res.status(200).json({
-                message:'Url generated',
-                url :shortId
-            })
-        }
+app.use(express.json());
 
-    }catch(error){
-       return res.status(500).send({
-            message:`Error:${error}`
-        })
+router.post("/url", async (req, res) => {
+  try {
+    const inputUrl = req.body;
+    console.log(inputUrl);
+    if (!inputUrl.url) {
+      return res.status(400).json({
+        message: "Url not found ",
+      });
     }
- 
+    const existingUrl = await UrlModel.findOne({ mainurl: inputUrl.url });
+    if (existingUrl) {
+      return res.send({
+        message: `Short URL already exist ${existingUrl.shortUrl}`,
+      });
+    }
+      console.log("newstage");
+
+      const id = shortUuid.generate();
+      const shortId =id.slice(0,8)
+      console.log(shortId);
+      
+      await UrlModel.create({
+        mainurl: inputUrl.url,
+        shortUrl: shortId,
+      });
+      return res.status(200).json({
+        message: "Url generated",
+        url: shortId,
+      });
+    
+  } catch (error) {
+    return res.status(500).send({
+      message: `Error:${error}`,
+    });
+  }
 });
 
 module.exports = router;
